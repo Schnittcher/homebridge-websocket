b1e9899132cd29276bf5a32a8df03c7b4468ca18
Kai Schnittcher <kai.schnittcher@gmail.com>
2017-04-15 21:25:00 +0200

    Update Fix

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ var storagePath;
 
 module.exports = function(homebridge) {
   console.log("homebridge API version: " + homebridge.version);
-  
+
   Accessory = homebridge.platformAccessory;
-  
+
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   UUIDGen = homebridge.hap.uuid; // Universally Unique IDentifier
-  
+
   storagePath = homebridge.user.storagePath();
-  
+
   homebridge.registerPlatform(plugin_name, platform_name, WebsocketPlatform, true);
 }
 
@@ -31,10 +31,10 @@ function WebsocketPlatform(log, config, api) {
   this.log = log;
   this.accessories = {};
   this.hap_accessories = {};
-  
+
   this.log.debug("storagePath = %s", storagePath);
   this.log.debug("config = %s", JSON.stringify(config));
-  
+
   if (typeof(config) !== "undefined" && config !== null) {
     this.port = config.port || {"port": 4050};
   } else {
@@ -42,10 +42,10 @@ function WebsocketPlatform(log, config, api) {
     this.log("storagePath = %s", storagePath);
     process.exit(1);
   }
-     
+
   var plugin_version = Utils.readPluginVersion();
   this.log("%s v%s", plugin_name, plugin_version);
-  
+
   var params = {
     "log": this.log,
     "plugin_name": plugin_name,
@@ -58,20 +58,14 @@ function WebsocketPlatform(log, config, api) {
   }
   this.Websocket = new Websocket(params);
 
-  Utils.read_npmVersion(plugin_name, function(npm_version) {
-    if (npm_version > plugin_version) {
-      this.log("A new version %s is avaiable", npm_version);
-    }
-  }.bind(this));
-
   if (api) {
     this.api = api;
 
     this.api.on('didFinishLaunching', function() {
       this.log("Plugin - DidFinishLaunching");
-     
+
      this.Websocket.startServer();
-             
+
       this.log.debug("Number of chaced Accessories: %s", cachedAccessories);
       this.log("Number of Accessories: %s", Object.keys(this.accessories).length);
 
@@ -85,25 +79,25 @@ WebsocketPlatform.prototype.addAccessory = function(accessoryDef) {
   var name = accessoryDef.name;
   var ack, message;
   var isValid;
-  
+
   if (!this.accessories[name]) {
     var uuid = UUIDGen.generate(name);
-    
+
     var newAccessory = new Accessory(name, uuid);
     newAccessory.reachable = true;
     newAccessory.context.service_name = accessoryDef.service;
-    
+
     //this.log.debug("addAccessory UUID = %s", newAccessory.UUID);
-    
+
     var i_accessory = new WebsocketAccessory(this.buildParams(accessoryDef));
     isValid = i_accessory.addService(newAccessory);
     if (isValid) {
       i_accessory.configureAccessory(newAccessory);
-      
+
       this.accessories[name] = i_accessory;
       this.hap_accessories[name] = newAccessory;
       this.api.registerPlatformAccessories(plugin_name, platform_name, [newAccessory]);
-      
+
       ack = true;
       message =  "accessory '" + name + "' is added.";
     } else {
@@ -121,25 +115,25 @@ WebsocketPlatform.prototype.addAccessory = function(accessoryDef) {
 WebsocketPlatform.prototype.configureAccessory = function(accessory) {
 
   //this.log.debug("configureAccessory %s", JSON.stringify(accessory.services, null, 2));
-  
+
   cachedAccessories++;
   var name = accessory.displayName;
   var uuid = accessory.UUID;
-    
+
   var accessoryDef = {};
   accessoryDef.name = name;
   accessoryDef.service = accessory.context.service_name;
-  
+
   if (this.accessories[name]) {
     this.log.error("configureAccessory %s UUID %s already used.", name, uuid);
     process.exit(1);
   }
-  
+
   accessory.reachable = true;
-    
+
   var i_accessory = new WebsocketAccessory(this.buildParams(accessoryDef));
   i_accessory.configureAccessory(accessory);
-  
+
   this.accessories[name] = i_accessory;
   this.hap_accessories[name] = accessory;
 }
@@ -147,10 +141,10 @@ WebsocketPlatform.prototype.configureAccessory = function(accessory) {
 WebsocketPlatform.prototype.removeAccessory = function(name) {
 
   var ack, message;
-  
+
   if (typeof(this.accessories[name]) !== "undefined") {
     this.log.debug("removeAccessory '%s'", name);
-    
+
     this.api.unregisterPlatformAccessories(plugin_name, platform_name, [this.hap_accessories[name]]);
     delete this.accessories[name];
     delete this.hap_accessories[name];
@@ -169,7 +163,7 @@ WebsocketPlatform.prototype.getAccessories = function(name) {
   var accessories = {};
   var def = {};
   var service, characteristics;
-  
+
   switch (name) {
     case "all":
       for (var k in this.accessories) {
@@ -180,7 +174,7 @@ WebsocketPlatform.prototype.getAccessories = function(name) {
         accessories[k] = def;
       }
     break;
-    
+
     default:
       service = this.accessories[name].service_name;
       characteristics =  this.accessories[name].i_value;
@@ -204,4 +198,3 @@ WebsocketPlatform.prototype.buildParams = function (accessoryDef) {
   //this.log.debug("configureAccessories %s", JSON.stringify(params.accessory_config));
   return params;
 }
-
